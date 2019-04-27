e9b72912e8d4b14db87afb96b60307940735930e
zhaojun0193 <zhaojun0193@163.com>
2019-04-27 14:57:26 +0800

    修改配置

--- hello-dubbo-provider/src/main/java/com/zhaojun/hello/dubbo/provider/service/DemoServiceImpl.java
+++ hello-dubbo-provider/src/main/java/com/zhaojun/hello/dubbo/provider/service/DemoServiceImpl.java
@@ -2,14 +2,12 @@ package com.zhaojun.hello.dubbo.provider.service;
 
 import com.zhaojun.hello.dubbo.api.DemoService;
 import org.apache.dubbo.config.annotation.Service;
-import org.springframework.stereotype.Component;
 
 /**
  * @author zhaojun0193
  * @date 2019/4/27
  */
 @Service(version = "${demo.service.version}")
-@Component
 public class DemoServiceImpl implements DemoService {
 
     @Override
